81ac3d4b49975bda43842cc148a00a65164525d2
othAs-dev <othmane.developer@gmail.com>
2023-10-22 21:27:25 +0200

    feat: building login view

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideHttpClient} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideRouter, withComponentInputBinding} from "@angular/router";
 import {routes} from "./app.routes";
+import {FormlyPresetModule} from "@ngx-formly/core/preset";
+import {FormlyMaterialModule} from '@ngx-formly/material';
+import {FormlyModule} from "@ngx-formly/core";
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes, withComponentInputBinding()),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(
+      FormlyPresetModule,
+      FormlyModule.forRoot(),
+      FormlyMaterialModule,
+    ),
   ],
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from "@angular/material/card";
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormlyFieldConfig, FormlyFormOptions, FormlyModule} from "@ngx-formly/core";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, FormlyModule, MatButtonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less'],
   standalone: true,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {
+    formState: {
+      awesomeIsForced: false,
+    },
+  };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'text',
+      type: 'input',
+      props: {
+        label: 'Adresse e-mail',
+        placeholder: 'john-doe@gmail.com',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      props: {
+        label: 'Mot de passe',
+        placeholder: '',
+        required: true,
+        type: 'password',
+      },
+    }
+  ];
+
+  submit() {
+    if (this.form.valid) {
+      alert(JSON.stringify(this.model));
+    }
+  }
+}

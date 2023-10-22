2fd181e3009c879d3d6437df6bac45be294e67cb
othAs-dev <othmane.developer@gmail.com>
2023-10-22 20:34:55 +0200

    chore update for fit with develop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [],
   template: '<router-outlet />',
   imports: [RouterOutlet],
   standalone: true

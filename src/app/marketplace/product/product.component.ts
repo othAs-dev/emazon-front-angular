6d019ff1f8039d692f06f50130e319e8b92d7233
othAs-dev <othmane.developer@gmail.com>
2023-11-09 17:42:14 +0100

    improve code

--- src/app/marketplace/product/product.component.ts
+++ src/app/marketplace/product/product.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {MatChipsModule} from "@angular/material/chips";
 import {MatExpansionModule} from "@angular/material/expansion";
 import {FooterComponent} from "../../shared/components/footer/footer.component";
-import {PageContent, pageContent} from "./product.constants";
+import {FaqItem, PageContent, pageContent, faqItems} from "./product.constants";
 import {MatTableModule} from "@angular/material/table";
 import {ProductCardComponent} from "../../shared/components/product-card/product-card.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, MatButtonModule, MatChipsModule, MatExpansionModule, FooterComponent, MatTableModule, ProductCardComponent],
+  imports: [CommonModule, MatButtonModule, MatChipsModule, MatExpansionModule, FooterComponent, MatTableModule, ProductCardComponent, RouterLink],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
   standalone: true,
 })
 export default class ProductComponent {
   protected readonly pageContent: PageContent = pageContent;
+  protected readonly faqItems: FaqItem[] = faqItems;
 }
--- src/app/marketplace/product/product.constants.ts
+++ src/app/marketplace/product/product.constants.ts
@@ -15,34 +15,21 @@ export interface ProductItem {
 }
 
 export interface PageContent {
+  urlImage: string;
   shortDescription: string;
   longDescription: string;
   chipOptionsColors: string[];
   chipOptionsStorage: string[];
-  faqItems: FaqItem[];
   tableData: TableRow[];
   products: ProductItem[];
 }
 
 export const pageContent: PageContent = {
+  urlImage: "../../../assets/mabook-air.jpeg",
   shortDescription: "Design tout écran. De toute beauté. Avec l’iPad Air, plongez dans tout ce que vous lisez, regardez ou créez. Son écran Liquid Retina 10,9 pouces dispose de tech­no­lo­gies avan­cées, telles que l’affi­cha­ge True Tone, la large gamme de couleurs P3 et un revê­te­ment antireflet.",
   longDescription: "Design tout écran. De toute beauté. Avec l’iPad Air, plongez dans tout ce que vous lisez, regardez ou créez. Son écran Liquid Retina 10,9 pouces dispose de tech­no­lo­gies avan­cées, telles que l’affi­cha­ge True Tone, la large gamme de couleurs P3 et un revê­te­ment antireflet. Touch ID est intégré au bouton supérieur. Vous pouvez ainsi utiliser votre empreinte digi­tale pour déver­­­rouiller votre iPad, vous connec­­ter à des apps ou effectuer des paie­­ments sécurisés avec Apple Pay. Et l’iPad Air se décline en cinq couleurs sublimes. Haut-parleurs stéréo sur les côtés de l’iPad AirDéverrouillez, connectez-vous et payez avec Touch ID.",
   chipOptionsColors: ['Gris sidéral', 'Bleu', 'Rose', 'Lumière stellaire'],
   chipOptionsStorage: ['128 Go', '256 Go', '512 Go', '1064 Go'],
-  faqItems: [
-    {
-      question: 'Garanties & Assistance',
-      answer: 'En cas de panne le produit est réparé sans frais. Si la réparation n’est pas possible, le produit est échangé. Olé.'
-    },
-    {
-      question: 'Livraison',
-      answer: 'On vous livre entre 24 et 72h.'
-    },
-    {
-      question: 'Quels modes de paiement sont disponibles?',
-      answer: 'Emazon accepte les modes de paiement suivants : Carte Bleue, Visa, MasterCard. Nous acceptons également Paypal et Klarna et bien plus encore.'
-    },
-  ],
   tableData: [
     { label: 'Gamme', value: 'iPad Air' },
     { label: 'Communication sans fil', value: 'Wi-Fi' },
@@ -79,3 +66,17 @@ export const pageContent: PageContent = {
   ]
 };
 
+export const faqItems: FaqItem[] = [
+  {
+    question: 'Garanties & Assistance',
+    answer: 'En cas de panne le produit est réparé sans frais. Si la réparation n’est pas possible, le produit est échangé. Olé.'
+  },
+  {
+    question: 'Livraison',
+    answer: 'On vous livre entre 24 et 72h.'
+  },
+  {
+    question: 'Quels modes de paiement sont disponibles?',
+    answer: 'Emazon accepte les modes de paiement suivants : Carte Bleue, Visa, MasterCard. Nous acceptons également Paypal et Klarna et bien plus encore.'
+  },
+];

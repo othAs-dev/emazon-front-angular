cd9ab3478337ad0c0b1d2a446555165720d75e75
othAs-dev <othmane.stig@gmail.com>
2023-12-25 17:32:54 +0100

    WIP

--- src/app/marketplace/product/product.component.ts
+++ src/app/marketplace/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {MatChipsModule} from "@angular/material/chips";
